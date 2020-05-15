27081c2702a581f6ee5bc062f9aa662f8b90865a
anthony.araujo <anthony.araujo@exxis-group.com>
2020-05-15 05:28:55 -0600

    primer deploy

---
+++ precache-manifest.2b8168723c357140fadf86266d4da0fc.js
@@ -0,0 +1,190 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80d2211ad00dfe4f8d776c15d4aac341",
+    "url": "/hakan-solutions/lima-retail/index.html"
+  },
+  {
+    "revision": "56405e673353c75e7569",
+    "url": "/hakan-solutions/lima-retail/static/css/2.e650959b.chunk.css"
+  },
+  {
+    "revision": "fb79978da6f645184164",
+    "url": "/hakan-solutions/lima-retail/static/css/main.1c2dcec6.chunk.css"
+  },
+  {
+    "revision": "56405e673353c75e7569",
+    "url": "/hakan-solutions/lima-retail/static/js/2.932f00a1.chunk.js"
+  },
+  {
+    "revision": "b05f692723b10304b96ec948f6a4b884",
+    "url": "/hakan-solutions/lima-retail/static/js/2.932f00a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75d254df18e0425aaa79",
+    "url": "/hakan-solutions/lima-retail/static/js/3.ff52e2f2.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/hakan-solutions/lima-retail/static/js/3.ff52e2f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba3e8b3d5e187ab09aaf",
+    "url": "/hakan-solutions/lima-retail/static/js/4.61ada350.chunk.js"
+  },
+  {
+    "revision": "b966ba5fdff8cc32784f",
+    "url": "/hakan-solutions/lima-retail/static/js/5.c2a453db.chunk.js"
+  },
+  {
+    "revision": "b798cf754325816d0241",
+    "url": "/hakan-solutions/lima-retail/static/js/6.460f2000.chunk.js"
+  },
+  {
+    "revision": "98afabba804628d2a833",
+    "url": "/hakan-solutions/lima-retail/static/js/7.761fb223.chunk.js"
+  },
+  {
+    "revision": "f52f9a31bc1cbdb2d4fa",
+    "url": "/hakan-solutions/lima-retail/static/js/8.e1cd3343.chunk.js"
+  },
+  {
+    "revision": "39763edf2f0a4f54b7c3",
+    "url": "/hakan-solutions/lima-retail/static/js/9.fc0b78b2.chunk.js"
+  },
+  {
+    "revision": "fb79978da6f645184164",
+    "url": "/hakan-solutions/lima-retail/static/js/main.6d3a216a.chunk.js"
+  },
+  {
+    "revision": "e84a777b7b6207b7cc31",
+    "url": "/hakan-solutions/lima-retail/static/js/runtime-main.e342498e.js"
+  },
+  {
+    "revision": "b9e633f07681fcfa7099f1760c6e8dc7",
+    "url": "/hakan-solutions/lima-retail/static/media/muli-latin-200.b9e633f0.woff"
+  },
+  {
+    "revision": "c13b3c679483605b32341233e5aa4f04",
+    "url": "/hakan-solutions/lima-retail/static/media/muli-latin-200.c13b3c67.woff2"
+  },
+  {
+    "revision": "6fa84fa76e883a13da897f462961bfdb",
+    "url": "/hakan-solutions/lima-retail/static/media/muli-latin-200italic.6fa84fa7.woff2"
+  },
+  {
+    "revision": "daa58017a1f434922b590d692e6b2af7",
+    "url": "/hakan-solutions/lima-retail/static/media/muli-latin-200italic.daa58017.woff"
+  },
+  {
+    "revision": "e98ca76130ec6c448f108d3fa8404897",
+    "url": "/hakan-solutions/lima-retail/static/media/muli-latin-300.e98ca761.woff2"
+  },
+  {
+    "revision": "eccf01eadc7d7c1777ecfb36d561a0b3",
+    "url": "/hakan-solutions/lima-retail/static/media/muli-latin-300.eccf01ea.woff"
+  },
+  {
+    "revision": "433aeba82069b1765de9fe2e059ecaf2",
+    "url": "/hakan-solutions/lima-retail/static/media/muli-latin-300italic.433aeba8.woff"
+  },
+  {
+    "revision": "e2385ec378d43b5ad454c40e00103af1",
+    "url": "/hakan-solutions/lima-retail/static/media/muli-latin-300italic.e2385ec3.woff2"
+  },
+  {
+    "revision": "705bcc4dd1c37efca70d440041d944e8",
+    "url": "/hakan-solutions/lima-retail/static/media/muli-latin-400.705bcc4d.woff2"
+  },
+  {
+    "revision": "91288b87b7bbe6d6fbfb131d5dbacbf1",
+    "url": "/hakan-solutions/lima-retail/static/media/muli-latin-400.91288b87.woff"
+  },
+  {
+    "revision": "1e42c4d1b57bf46a137ffa2983ebdd8b",
+    "url": "/hakan-solutions/lima-retail/static/media/muli-latin-400italic.1e42c4d1.woff"
+  },
+  {
+    "revision": "543e731fffe3d8421819c5115a7f5141",
+    "url": "/hakan-solutions/lima-retail/static/media/muli-latin-400italic.543e731f.woff2"
+  },
+  {
+    "revision": "224bba330d827c890daccf803dcd5ab1",
+    "url": "/hakan-solutions/lima-retail/static/media/muli-latin-500.224bba33.woff"
+  },
+  {
+    "revision": "57d637872afea75f042b7d937b1db98c",
+    "url": "/hakan-solutions/lima-retail/static/media/muli-latin-500.57d63787.woff2"
+  },
+  {
+    "revision": "9f876a07aeab5afccedbd606fac378d7",
+    "url": "/hakan-solutions/lima-retail/static/media/muli-latin-500italic.9f876a07.woff2"
+  },
+  {
+    "revision": "d4c5908da6e36c6b0ecd92751bfe7ca2",
+    "url": "/hakan-solutions/lima-retail/static/media/muli-latin-500italic.d4c5908d.woff"
+  },
+  {
+    "revision": "5ea5ffaf259f9436753bf965e76e6e12",
+    "url": "/hakan-solutions/lima-retail/static/media/muli-latin-600.5ea5ffaf.woff"
+  },
+  {
+    "revision": "e840b5fe8105c3e65fda1c156335834e",
+    "url": "/hakan-solutions/lima-retail/static/media/muli-latin-600.e840b5fe.woff2"
+  },
+  {
+    "revision": "52b09d32a3d04d9fb5348cdb5c3fc230",
+    "url": "/hakan-solutions/lima-retail/static/media/muli-latin-600italic.52b09d32.woff"
+  },
+  {
+    "revision": "7db7f0f799440f2a628e36a74ab9303b",
+    "url": "/hakan-solutions/lima-retail/static/media/muli-latin-600italic.7db7f0f7.woff2"
+  },
+  {
+    "revision": "1d982ad1cb7695225c7c0a0cfc1b10b6",
+    "url": "/hakan-solutions/lima-retail/static/media/muli-latin-700.1d982ad1.woff2"
+  },
+  {
+    "revision": "c0a2a08d31879a5053d60aea0f5bb645",
+    "url": "/hakan-solutions/lima-retail/static/media/muli-latin-700.c0a2a08d.woff"
+  },
+  {
+    "revision": "08ea8fa20100d95518e18e84739bf109",
+    "url": "/hakan-solutions/lima-retail/static/media/muli-latin-700italic.08ea8fa2.woff2"
+  },
+  {
+    "revision": "4fbf29a657022481c0f868ed3ca62616",
+    "url": "/hakan-solutions/lima-retail/static/media/muli-latin-700italic.4fbf29a6.woff"
+  },
+  {
+    "revision": "10ca7e8e05c480f44b1be2f50cc6d025",
+    "url": "/hakan-solutions/lima-retail/static/media/muli-latin-800.10ca7e8e.woff"
+  },
+  {
+    "revision": "73541670fa9b16a54cd06c61c6a0a2c7",
+    "url": "/hakan-solutions/lima-retail/static/media/muli-latin-800.73541670.woff2"
+  },
+  {
+    "revision": "233121335c5f82380fec914d63e19221",
+    "url": "/hakan-solutions/lima-retail/static/media/muli-latin-800italic.23312133.woff2"
+  },
+  {
+    "revision": "6b57e7c3279568d43a93622e0e60c25f",
+    "url": "/hakan-solutions/lima-retail/static/media/muli-latin-800italic.6b57e7c3.woff"
+  },
+  {
+    "revision": "c805b62edeb6f97c196e3581d5c0007c",
+    "url": "/hakan-solutions/lima-retail/static/media/muli-latin-900.c805b62e.woff2"
+  },
+  {
+    "revision": "d84d7000d212375e4da6cb6197c01245",
+    "url": "/hakan-solutions/lima-retail/static/media/muli-latin-900.d84d7000.woff"
+  },
+  {
+    "revision": "14bc5b4bd274bab8694cfb91f0feb567",
+    "url": "/hakan-solutions/lima-retail/static/media/muli-latin-900italic.14bc5b4b.woff"
+  },
+  {
+    "revision": "fb9b3ba4a7ed8525fbde97b3f5518eb3",
+    "url": "/hakan-solutions/lima-retail/static/media/muli-latin-900italic.fb9b3ba4.woff2"
+  }
+]);
